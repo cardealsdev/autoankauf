b3b01d37866d60459c22db7893523ef266165a04
Abdulrahman Alallaqi <pow22000@adobe.com>
2023-02-18 12:24:48 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slideshow-item");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
